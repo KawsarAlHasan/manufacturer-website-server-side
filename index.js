95078869efd0d1829cf803d93b00dd82f7a7c982
KawsarAlHasan <kawsaralhasan.420@gmail.com>
2023-07-13 01:16:28 +0600

    create user and admin

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const cors = require('cors');
-const jwt = require('jsonwebtoken');
-require('dotenv').config();
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -11,17 +11,21 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xh2az.mongodb.net/?retryWrites=true&w=majority`;
 
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 function verifyJWT(req, res, next) {
   const authHeader = req.headers.authorization;
   if (!authHeader) {
-    return res.status(401).send({ message: 'UnAuthrized access' })
+    return res.status(401).send({ message: "UnAuthrized access" });
   }
-  const token = authHeader.split(' ')[1];
+  const token = authHeader.split(" ")[1];
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
     if (err) {
-      return res.status(403).send({ message: 'fobidden access' });
+      return res.status(403).send({ message: "fobidden access" });
     }
     req.decoded = decoded;
     next();
@@ -31,18 +35,18 @@ function verifyJWT(req, res, next) {
 async function run() {
   try {
     await client.connect();
-    const carPartsCollection = client.db('car_parts').collection('parts');
-    const purchaseCollection = client.db('car_parts').collection('purchase');
-    const usersCollection = client.db('car_parts').collection('users');
+    const carPartsCollection = client.db("car_parts").collection("parts");
+    const purchaseCollection = client.db("car_parts").collection("purchase");
+    const usersCollection = client.db("car_parts").collection("users");
 
-    app.get('/carParts', async (req, res) => {
+    app.get("/carParts", async (req, res) => {
       const query = {};
       const cursor = carPartsCollection.find(query);
       const carParts = await cursor.toArray();
       res.send(carParts);
     });
 
-    app.get('/carParts/:id', async (req, res) => {
+    app.get("/carParts/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
       const carParts = await carPartsCollection.findOne(query);
@@ -50,19 +54,34 @@ async function run() {
     });
 
     //Delete
-    app.delete('/carParts/:id', async (req, res) => {
+    app.delete("/carParts/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
       const result = await carPartsCollection.deleteOne(query);
       res.send(result);
     });
 
-    app.get('/user', async (req, res) => {
+    // get users
+    app.get("/users", async (req, res) => {
       const users = await usersCollection.find().toArray();
       res.send(users);
-    })
+    });
 
-    app.put('/user/:email', async (req, res) => {
+    app.get("/users/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email };
+      const user = await usersCollection.findOne(query);
+      res.send({ isAdmin: user?.role === "admin" });
+    });
+
+    // users create
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.put("/user/:email", async (req, res) => {
       const email = req.params.email;
       const user = req.body;
       const filter = { email: email };
@@ -70,29 +89,37 @@ async function run() {
       const updateDoc = {
         $set: user,
       };
-      const result = await usersCollection.updateOne(filter, updateDoc, options);
-      const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+      const result = await usersCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      const token = jwt.sign(
+        { email: email },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: "1h" }
+      );
       res.send({ result, token });
-    })
+    });
 
     // parts post
-    app.post('/addParts', async (req, res) => {
+    app.post("/addParts", async (req, res) => {
       const newParts = req.body;
       const result = await carPartsCollection.insertOne(newParts);
       res.send(result);
     });
 
     //my purchase
-    app.get('/purchase', async (req, res) => {
+    app.get("/purchase", async (req, res) => {
       const email = req.query.email;
       const query = { email: email };
       const cursor = purchaseCollection.find(query);
       const orders = await cursor.toArray();
       res.send(orders);
-    })
+    });
 
     //delete
-    app.delete('/purchase/:id', async (req, res) => {
+    app.delete("/purchase/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
       const result = await purchaseCollection.deleteOne(query);
@@ -100,7 +127,7 @@ async function run() {
     });
 
     //post my purchase
-    app.post('/purchase', async (req, res) => {
+    app.post("/purchase", async (req, res) => {
       const purchase = req.body;
       const partsQuantity = purchase.partsQuantity;
       const minimumQuantity = purchase.minimumQuantity;
@@ -108,23 +135,19 @@ async function run() {
       if (partsQuantity >= userQuantity && userQuantity >= minimumQuantity) {
         const result = await purchaseCollection.insertOne(purchase);
         return res.send({ success: true, result });
-      }
-      else {
+      } else {
         return res.send({ success: false });
       }
     });
-
-  }
-  finally {
-
+  } finally {
   }
 }
 run().catch(console.dir);
 
-app.get('/', (req, res) => {
-  res.send('Hello manufacturer website')
-})
+app.get("/", (req, res) => {
+  res.send("Hello manufacturer website");
+});
 
 app.listen(port, () => {
-  console.log(`Manufacturer website app listening on port ${port}`)
-})
+  console.log(`Manufacturer website app listening on port ${port}`);
+});
